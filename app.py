6026eba6f23d38e05ff0c3f056a10af436fea9d9
LukasPalm <86203896+LukasPalm@users.noreply.github.com>
2024-06-10 18:44:57 +0200

    MVP done

--- app.py
+++ app.py
@@ -1,27 +1,75 @@
-from flask import Flask, render_template, make_response, request
+from flask import Flask, render_template, make_response, request, send_file, abort
+import shutil, os, json, ast
 
 
 app = Flask(__name__)
+def getnavbars():
+    data = []
+    navbars = os.listdir('static/assets/templates/navbars/')
 
+    for item in navbars:
+        json_data = json.load(open('static/assets/templates/navbars/' + item + "/data.json", "r"))
+        data.append(json_data)
+    return data
+            
+
+        
+
+elements = {
+    'navbars': getnavbars()
+}
 
 @app.route('/')
 def index():
     resp = make_response(render_template('index.html'))
-    resp.set_cookie('site-pieces', '{}')
+    resp.set_cookie('site-pieces', '[]')
     return resp
 
 @app.route('/create')
 def create():
-    return render_template('create.html')
+    resp = make_response(render_template('create.html'))
+    resp.set_cookie('site-pieces', '[]')
+    resp.set_cookie('selection', '')
+    return resp
+    
 
-@app.route('/add')
-def add():
-    id = request.args.get('id')
-    if id:
-        print("Adding item with id: " + str(id))
-        return "Adding item with id: " + str(id)
+
+@app.route('/get-items')
+def get_items():
+    target = request.args.get('target')
+    html = ""
+    if target:
+        if target in elements:
+            print(elements[target])
+            for item in elements[target]:
+                itempath = item['path']
+                print(itempath)
+                html += f'<div class="col-xl-6 col-lg-6 col-md-6 col-12 my-3"><img src="{itempath}image.png" alt="navbar" width="100%" onclick="SelectItem(\'{itempath}\', this)" class="option-item"></div>\n'
+            return html
+        return f"No {target} found"
     else:
-        return "No id provided", 400
+        return "No target provided", 400
+
+
+@app.route('/download')
+def download():
+    html = ""
+    pieces = ast.literal_eval(request.cookies.get('site-pieces'))
+    try:
+        for piece in pieces:
+            html += open(piece + "code.html", 'r').read()
+        with open('created-site/index.html', 'w') as file:
+            base = open('static/assets/templates/base/base.html', 'r').read()
+            finished = base.replace("{content}", html)
+            file.write(finished)
+            file.close()
+
+
+        shutil.make_archive('created-site', 'zip', 'created-site')
+        return send_file('created-site.zip', as_attachment=True)
+    
+    except FileNotFoundError as e:
+        print(f"Error: {e}")
 
 
 if __name__ == '__main__':
--- static/site-files/script.js
+++ static/site-files/script.js
@@ -10,5 +10,47 @@ for (var i = 0; i < dropdown_items.length; i++) {
 
   dropdown_items[i].addEventListener("click", function() {
       document.getElementById("chooseCategory").innerHTML = this.innerHTML;
+      let target = this.innerHTML.toLowerCase();
+      fetch("http://127.0.0.1:1337/get-items?target=" + target)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok ' + response.statusText);
+                    }
+                    return response.text();
+                })
+                .then(data => {
+                    document.getElementById("optionsSpace").innerHTML = data;
+                })
+                .catch(error => {
+                    console.error('There was a problem with the fetch operation:', error);
+                });
   });
 }
+
+function SelectItem(path, thisItem) {
+  document.cookie = "selection=" + path + "; sameSite=Strict;";
+  items = document.getElementsByClassName("option-item");
+  for (var i = 0; i < items.length; i++) {
+    items[i].style.boxShadow = "none";
+  }
+  thisItem.style.boxShadow = "0px 0px 10px 2px #03DAC6";
+
+}
+
+function add() {
+  if (document.cookie.split("selection=")[1].length == 0) {
+    alert("Please select an item before pressing add.");
+  }
+  var selection = document.cookie.split("selection=")[1];
+  var sitePieces = JSON.parse(document.cookie.split("site-pieces=")[1].split(";")[0]);
+  sitePieces.push(selection);
+  document.cookie = "site-pieces=" + JSON.stringify(sitePieces) + "; sameSite=Strict;";
+  document.cookie = "selection=; sameSite=Strict;";
+
+  alert("Item added to site!");
+  items = document.getElementsByClassName("option-item");
+  for (var i = 0; i < items.length; i++) {
+    items[i].style.boxShadow = "none";
+  }
+}
+
